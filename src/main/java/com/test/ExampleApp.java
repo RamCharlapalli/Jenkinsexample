8a2b8e19b6487d6e725c8a7c3a4caf4cffcaa862
Ram <ramcharlapalli@gmail.com>
2025-01-31 22:12:53 -0500

    final commit

--- src/main/java/com/test/ExampleApp.java
+++ src/main/java/com/test/ExampleApp.java
@@ -4,7 +4,7 @@ public class ExampleApp {
 public static void main(String[] args) {
 	System.out.println("------------------------------");
 	System.out.println("QA toiuch !!");
-	System.out.println("Smarter test automatin for QA teams!!");
+	System.out.println("Smarter test automatin for QA teams!!!!!");
 	System.out.println("----------------------");
 }
 }
